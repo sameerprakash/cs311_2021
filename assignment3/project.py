a860c09554406a0f2ead8524edebfa00bad6b7b4
sameerprakash <66964618+sameerprakash@users.noreply.github.com>
2021-10-31 20:54:01 -0700

    Update project.py

--- assignment3/project.py
+++ assignment3/project.py
@@ -1,13 +1,19 @@
 import argparse
-
+import json
 parser = argparse.ArgumentParser()
 parser.add_argument('--init', help='called when new game')
 parser.add_argument('--iterations', help='number of iterations in game')
 parser.add_argument('--last_opponent_move', help='last opponent move')
 
 args = parser.parse_args()
+mymoves = open("mymoves.json")
+moves = json.load(mymoves)
 
-#print(args.last_opponent_move)
-
-print( random.choice(['confess', 'silent']) )
-
+if(args.init == "true"):
+  moves = ["silent"]
+elif(args.last_opponent_move == "zero"):
+  print(moves.pop())
+else:
+  moves.append(args.last_opponent_move)
+  moves.pop()
+json.dump(moves,mymoves)
